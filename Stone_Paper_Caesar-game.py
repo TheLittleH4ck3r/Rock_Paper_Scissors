3631b70fe1aa0e00be322eafacf5bf27beb40d48
iamM4ddy <79082257+iamM4ddy@users.noreply.github.com>
2021-08-08 11:11:45 +0530

    Update Stone_Paper_Caesar-game.py

--- Stone_Paper_Caesar-game.py
+++ Stone_Paper_Caesar-game.py
@@ -1,46 +1,38 @@
 import random
+player = False
+cpu_score = 0
+player_score = 0
+while True:
+    choices = ["Rock", "Paper", "Scissors"]
+    computer = random.choice(choices)
 
-def gameWin(computer, you):
-    if computer == you:
-        return None
-    elif computer == 'stone':
-        if you =='caesar':
-            return False
-        elif you =='paper':
-            return True
-    elif computer == 'paper':
-        if you=='stone':
-            return False
-        elif you =='caesar':
-            return True
-    elif computer == 'caesar':
-        if you =='stone':
-            return True
-        elif you=='paper':
-            return False
-
-
-
-print("Computer True : Stone, Paper, or Caesar?")
-randNo = random.randint(1, 3)
-if randNo == 1:
-    computer = 'stone'
-elif randNo == 2:
-    computer = 'paper'
-elif randNo == 3:
-    computer = 'caesar'
-
-you = input ("Your turn: Stone, Paper, or Caesar?\n")
-a = gameWin(computer, you)
-
-print(f"Computer chose {computer}")
-print(f"You chose {you}")
- 
-if a ==None:
-    print("The game is tie!")
-elif a :
-    print("You Win!")
-else:
-    print("You Lose!")
-
-    
+    player = input("Rcok, Paper or Scissors?").capitalize()
+    # Conditions of Rock, Paper and Scissors
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score +=1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "covers", computer)
+            player_score+=1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose..", computer, "smashes", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "cut", computer)
+            player_score+=1
+    elif player == 'End':
+        print("Final Scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"Player:{player_score}")
+        break
